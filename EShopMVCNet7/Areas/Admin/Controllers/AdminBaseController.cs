8dfcd12b01bfaa49c1b9fbf63eaefbe95e309315
DESKTOP-9SAS8HE\DELL <khanhhung89890@gmail.com>
2023-09-08 21:56:05 +0700

    Update Page UserApp

--- EShopMVCNet7/Areas/Admin/Controllers/AdminBaseController.cs
+++ EShopMVCNet7/Areas/Admin/Controllers/AdminBaseController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EShopMVCNet7.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EShopMVCNet7.Areas.Admin.Controllers
 {
     public class AdminBaseController : Controller
     {
+        protected const int PER_PAGE = 20;
+        protected EshopDbContext _db;
 
+        public AdminBaseController(EshopDbContext db)
+        {
+            _db = db;
+        }
     }
 }
---
+++ EShopMVCNet7/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using EShopMVCNet7.Models;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace EShopMVCNet7.Areas.Admin.Controllers
+{
+    //kế thừa của adminbasecontroller
+    public class UserController : AdminBaseController
+    {
+        public UserController(EshopDbContext db) : base(db)
+        {
+        }
+
+        //phân trang
+        public IActionResult Index(int page = 1)
+        {
+            var data = _db.AppUsers
+                .OrderByDescending(x => x.Id)
+                .ToPagedList(page, PER_PAGE);//được viết in hoa;;; phân trang all page
+            return View(data);//chuyển data qua view
+        }
+        //update
+        public IActionResult Update(int id)
+        {
+            var data = _db.AppUsers.Find(id);
+
+            if (data == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(data);
+        }
+    }
+}
