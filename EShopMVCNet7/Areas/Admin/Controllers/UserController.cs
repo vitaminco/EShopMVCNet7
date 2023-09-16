82f72fa63c9b5b9932c232bdd32b6c3560f518f2
DESKTOP-9SAS8HE\DELL <khanhhung89890@gmail.com>
2023-09-16 13:44:48 +0700

    add create page with AD and Customer

--- EShopMVCNet7/Areas/Admin/Controllers/UserController.cs
+++ EShopMVCNet7/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using EShopMVCNet7.Models;
+﻿using EShopMVCNet7.Common;
+using EShopMVCNet7.Models;
 using Microsoft.AspNetCore.Mvc;
 using X.PagedList;
 
@@ -10,6 +11,7 @@ namespace EShopMVCNet7.Areas.Admin.Controllers
         public UserController(EshopDbContext db) : base(db)
         {
         }
+        [HttpGet]
 
         //phân trang
         public IActionResult Index(int page = 1)
@@ -18,6 +20,39 @@ namespace EShopMVCNet7.Areas.Admin.Controllers
                 .OrderByDescending(x => x.Id)
                 .ToPagedList(page, PER_PAGE);//được viết in hoa;;; phân trang all page
             return View(data);//chuyển data qua view
+        }
+        public IActionResult Create() => View();
+        //đăng kí
+        [HttpPost]
+        public IActionResult Create(AppUser user, [FromServices] EshopDbContext db)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(user);
+            }
+            //chuẩn hóa username và email 
+            user.Username = user.Username.ToLower().Trim();
+            user.Email = user.Email.ToLower().Trim();
+            //check username và email đã tồn tại chưa
+            var exists = db.AppUsers.Any(u => u.Email == user.Email || u.Username == user.Username);
+            if (exists)
+            {
+                ModelState.AddModelError("", "Email hoặc tên đăng nhập đã được sử dụng!");
+                return View(user);
+            }
+            //hash mật khẩu
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+            //khai báo biến sử lý data
+
+            /*user.Role = UserRole.ROLECUSTOMER;*/
+            user.Role = user.Role;
+            user.BlockedTo = null;
+
+            db.AppUsers.Add(user);
+            db.SaveChanges();
+
+            return RedirectToAction(nameof(Create));//nameof là để ở trên đổi tên thì dưới nó cx đổi theo
         }
         //update
         public IActionResult Update(int id)
