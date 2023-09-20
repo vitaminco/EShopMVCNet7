39828be54128e194c3b9d0166775509fab7c352f
DESKTOP-9SAS8HE\DELL <khanhhung89890@gmail.com>
2023-09-20 19:41:49 +0700

    modal

---
+++ EShopMVCNet7/wwwroot/js/admin/category.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener("alpine:init", () => {
+    Alpine.data("category", () => ({
+        _list: [],
+
+        init() {
+            fetch("/Admin/Category/ListAll")
+                .then(x => x.json())
+                .then(json => {
+                    this._list = json;
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }));
+})
